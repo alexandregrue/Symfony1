5f537dcbdda0a24582015ffc38b2202c4d03a465
Alexandre Grué <alexandre.grue@hotmail.com>
2021-11-30 11:49:40 +0100

    doctrine add in database

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program", name="program")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program/", name="program_index")
+     * @Route("/", name="program_index")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", requirements={"id"="\d+"}, methods={"GET"}, name="program_show")
+     */
+    public function show($id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id_program' => $id,
         ]);
     }
 }
