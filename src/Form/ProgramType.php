b2f672c7340838e6a99badccc88dce4bc8b35d02
Alexandre Grué <alexandre.grue@hotmail.com>
2021-12-06 10:36:10 +0100

    add actor field in addprogram

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Program;
 use App\Entity\Category;
+use App\Entity\Actor;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -27,6 +28,13 @@ class ProgramType extends AbstractType
             ->add('country', TextType::class)
             ->add('year', IntegerType::class)
             ->add('category', EntityType::class, ['class' => Category::class, 'choice_label' => 'name'])
+            ->add('actors', EntityType::class, [
+                'class' => Actor::class,
+                'choice_label' => 'name',
+                'multiple' => true,
+                'expanded' => true,
+                'by_reference' => false,
+            ])
         ;
     }
 
